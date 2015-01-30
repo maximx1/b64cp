dd69fe4dc566c5fbe6a2ab76e6469311d843cc8a
Justin Walrath <walrathjaw@gmail.com>
2015-01-29 23:36:29 -0600

    original method didn't work.. Had to go windows specific for the time being.

--- b64cp.py
+++ b64cp.py
@@ -1,25 +1,33 @@
 import sys
 import base64
+import os
+from threading import Thread
 
 try:
 	from Tkinter import Tk
 except ImportError:
 	from tkinter import Tk
-
-
+	
 def encodeToBase64(originalString):
-	return base64.b64encode(originalString).encode("utf-8")
+	return base64.b64encode(bytes(originalString, "utf-8")).decode("utf-8")
 
-def runner():
-	tk = Tk()
-	tk.withdraw()
-	originalString = ''
+def unicornMethod(cp, b64string):
+	cp.clipboard_clear()
+	cp.clipboard_append(b64string)
+	cp.destroy()
+	
+def windowsSpecificAlternative(b64string):
+	os.system("echo " + b64string + " | clip")
+	
+def unicornMethod():
+	cp = Tk()
+	cp.withdraw()
+	originalString = ""
 	if len(sys.argv) == 1:
-		originalString = tk.selection_get(selection = "CLIPBOARD")
+		originalString = cp.selection_get(selection = "CLIPBOARD")
 	else:
 		originalString = sys.argv[1]
-	tk.clipboard_clear()
-	tk.clipboard_append(encodeToBase64(originalString))
-	tk.destroy()
-	
-runner()
+	b64string = encodeToBase64(originalString)
+	windowsSpecificAlternative(b64string)
+
+unicornMethod()
